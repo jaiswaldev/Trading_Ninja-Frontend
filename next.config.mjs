fd6d3d0ee79e0fd6d90946032ebde8c51b1ccd3e
Dev Jaiswal <jaiswaldev479@gmail.com>
2025-02-11 11:11:37 +0530

    error fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, 
+    },
+    experimental: {
+      appDir: true, 
+    },
+    reactStrictMode: true, 
+    webpack: (config) => {
+      config.resolve.fallback = {
+        fs: false, 
+        path: false,
+        os: false,
+      };
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
